1ac9fa72c11ee6a148749a907171a163552ef7c4
Althea Smith <ars_610@yahoo.com>
2022-11-03 21:45:40 -0400

    Beginnings of home layout

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,21 +3,34 @@ package com.example.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.finalproject.databinding.ActivityMainBinding
+import com.example.finalproject.ui.HomeFragment
 import com.example.finalproject.ui.RestaurantDetails
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val homeFragTag = "homeFragTag"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
+        addHomeFragment()
 
         activityMainBinding.contentMain.viewResults.setOnClickListener {
             launchRestaurantDetailActivity()
         }
     }
 
+    private fun addHomeFragment() {
+        supportFragmentManager.commit {
+            add(R.id.main_frame, HomeFragment.newInstance(), homeFragTag)
+        }
+    }
 
     private fun launchRestaurantDetailActivity () {
         val restaurantDetailIntent = Intent(this, RestaurantDetails::class.java)
---
+++ app/src/main/java/com/example/finalproject/ui/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.finalproject.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.finalproject.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance(): HomeFragment {
+            return HomeFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
