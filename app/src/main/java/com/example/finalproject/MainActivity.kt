2a8bfba126dc3d00d5b4c4edf63fc0703a8adca3
Althea Smith <ars_610@yahoo.com>
2022-11-06 12:38:36 -0500

    Logout menu option implemented

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.commit
 import com.example.finalproject.databinding.ActivityMainBinding
 import com.example.finalproject.ui.HomeFragment
 import com.example.finalproject.ui.RestaurantDetails
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,10 +34,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.logout -> {
+            logout()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun addHomeFragment() {
         supportFragmentManager.commit {
             add(R.id.main_frame, HomeFragment.newInstance(), HomeFragment.tag)
         }
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+
+        // Go back to login page
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setIsSmartLockEnabled(false)
+            .build()
+
+        signInLauncher.launch(signInIntent)
     }
 
     private fun launchRestaurantDetailActivity () {
--- app/src/main/java/com/example/finalproject/ui/HomeFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/HomeFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.finalproject.R
 import com.example.finalproject.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class HomeFragment: Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -28,6 +31,14 @@ class HomeFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Todo probably need to store this as Live Data. Home Fragment is created before login is completed.
+        val currentUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+        if (currentUser?.displayName != null) {
+            binding.welcomeMessage.text = String.format(getString(R.string.welcome_message), currentUser.displayName!!.substringBefore(" "))
+        } else {
+            binding.welcomeMessage.text = "Welcome!"
+        }
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
